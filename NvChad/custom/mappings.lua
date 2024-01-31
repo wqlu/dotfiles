87abe8b66ebc091519182fdf59171b806a80d46f
wqlu <wq.lu@outlook.com>
2024-01-31 09:45:57 +0800

    NvChad:add custom mapping

--- NvChad/custom/mappings.lua
+++ NvChad/custom/mappings.lua
@@ -1,6 +1,19 @@
 ---@type MappingsTable
 local M = {}
 
+M.disabled = {
+  t = {
+    ["<A-h>"] = "",
+    ["<A-v>"] = ""
+  },
+  n = {
+    ["<A-h>"] = "",
+    ["<A-v>"] = "",
+    ["<leader>h"] = "",
+    ["<leader>v"] = ""
+  }
+}
+
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
@@ -15,8 +28,57 @@ M.general = {
 
   },
   v = {
-    [">"] = { ">gv", "indent"},
+    [">"] = { ">gv", "indent" },
   },
+}
+
+M.nvterm = {
+  t = {
+    -- toggle in terminal mode
+    ["<A-->"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "Toggle horizontal term",
+    },
+
+    ["<A-\\>"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+      "Toggle vertical term",
+    },
+  },
+
+  n = {
+    ["<A-->"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "Toggle horizontal term",
+    },
+
+    ["<A-\\>"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+      "Toggle vertical term",
+    },
+
+    ["<leader>-"] = {
+      function()
+        require("nvterm.terminal").new "horizontal"
+      end,
+      "New horizontal term",
+    },
+
+    ["<leader>\\"] = {
+      function()
+        require("nvterm.terminal").new "vertical"
+      end,
+      "New vertical term",
+    },
+  }
 }
 
 -- more keybinds!
